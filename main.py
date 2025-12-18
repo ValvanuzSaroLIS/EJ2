d2e97b4b3f27e828b8dd6940013bfadef743deb1
DesarrolladorC <desarrolladorC@ejemplo.com>
2025-12-18 13:51:13 +0100

    Implementacion del metodo 1: DevC

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from astronomia.aliens import aliens
 from astronomia.planetas import planetas
 from astronomia.galaxias import galaxias
 
+def explora(a):
+    sitios_a_explorar = ["rio", "lago", "campo"]
+    print(f"el alien {a} explora {random.choice(sitios_a_explorar)}")
 
 def cosmos(a, p, g):
     for alien in a:
