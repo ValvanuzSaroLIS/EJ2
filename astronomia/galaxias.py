8d03c7c3026f6396d4b8a44bcfab119f8e9e4244
DesarrolladorC <desarrolladorC@ejemplo.com>
2025-12-18 13:18:39 +0100

    Subida de galaxias por DevC

--- astronomia/galaxias.py
+++ astronomia/galaxias.py
@@ -1,1 +1,13 @@
-
+#lista de galaxias
+galaxias = [
+    "Vía Láctea",
+    "Andrómeda",
+    "Galaxia del Sombrero",
+    "Galaxia del Remolino",
+    "Galaxia de Bode",
+    "Galaxia de la Escultora",
+    "Galaxia del Triángulo",
+    "Galaxia de la Osa Mayor",
+    "Galaxia de la Osa Menor",
+    "Galaxia de la Ballena"
+]
